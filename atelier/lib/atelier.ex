a05311bdd068daf6c4a788721b68e689beb14461
def-elixir <def.elixir@gmail.com>
2024-07-02 11:39:24 +0900

    open json file

--- atelier/lib/atelier.ex
+++ atelier/lib/atelier.ex
@@ -15,4 +15,25 @@ defmodule Atelier do
   def hello do
     :world
   end
+
+  def open_json_file(path) do
+    with {:ok, encoded} <- file_read(path),
+         {:ok, decoded} <- json_decode(encoded) do
+      {:ok, decoded}
+    end
+  end
+
+  defp json_decode(encoded) do
+    case Jason.decode(encoded) do
+      {:ok, decoded} -> {:ok, decoded}
+      {:error, _} -> {:error, :badfile}
+    end
+  end
+
+  defp file_read(path) do
+    case File.read(path) do
+      {:ok, contents} -> {:ok, contents}
+      {:error, _} -> {:error, :badfile}
+    end
+  end
 end
